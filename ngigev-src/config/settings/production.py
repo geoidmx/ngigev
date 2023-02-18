88198fc85fe1d889941d43c2b66a405d6d0e2024
arbekos <arbek_s13@hotmail.com>
2023-02-17 19:04:39 -0600

    static dirs

--- ngigev-src/config/settings/production.py
+++ ngigev-src/config/settings/production.py
@@ -28,10 +28,11 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ( BASE_DIR / "/static" )
+STATIC_ROOT = ( BASE_DIR / "../static" )
 
 STATICFILES_DIRS = [
-    BASE_DIR / "../static",
+    BASE_DIR / "../static/assets",
+    BASE_DIR / "../static/src",
 ]
 
 MEDIA_URL = '/media/'
